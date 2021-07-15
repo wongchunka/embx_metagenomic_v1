825a1bfcda2068ea0b42ee91529a8de5de06030d
Ubuntu <azureuser@omicd8av4.tolh10qp5cpurebkixtehezu3a.jx.internal.cloudapp.net>
2021-07-15 14:24:34 +0000

    Added bowtie2-build to viral reference genome downloading code.

--- 8_downloadrefgenome_v1.sh
+++ 8_downloadrefgenome_v1.sh
@@ -9,4 +9,5 @@ grep -E "$1" /home/azureuser/development/omic/assembly_summary_refseq.txt | cut 
 awk 'BEGIN{FS=OFS="/";filesuffix="genomic.fna.gz"}{ftpdir=$0;asm=$10;file=asm"_"filesuffix;print "wget "ftpdir,file" -O /home/azureuser/development/omic/data_refgen/temp.fna.gz\ngunzip /home/azureuser/development/omic/data_refgen/temp.fna.gz"}' /home/azureuser/development/omic/ftp_folder.txt > /home/azureuser/development/omic/download_fna_files.sh
 source /home/azureuser/development/omic/download_fna_files.sh
 mv /home/azureuser/development/omic/data_refgen/temp.fna "/home/azureuser/development/omic/data_refgen/$2.fna"
+bowtie2-build "/home/azureuser/development/omic/data_refgen/$2.fna" "/home/azureuser/development/omic/data_refgen/$2"
 fi
